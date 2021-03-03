d37047f0949a92995a7da7d810ce30cd6353bbdf
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-03-03 15:49:40 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Solution.java
@@ -0,0 +1,11 @@
+/* 
+Product of 10 numbers
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int answer = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+        System.out.println(answer);
+    }
+}
